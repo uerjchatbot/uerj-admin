58f76774042780c690c5d90b35697807b69e2fec
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-10-24 14:48:53 -0300

    feat: local storage hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+type Props = {
+  key: string;
+  initialValue: any;
+};
+
+export const useLocalStorage = ({ key, initialValue }: Props) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+
+    try {
+      const item = window.localStorage.getItem(key);
+
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: any) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
