84fe6294baf78560926d50e1c58ede77c5f847de
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-10-24 14:51:14 -0300

    feat: app contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { ThemeProvider } from "styled-components";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import RoutesComponent from "@/routes/routes.component";
 import GlobalStyle from "@/styles/global.style";
 import Theme from "@/styles/theme";
+import Loading from "./components/Loading";
+import { AppProvider } from "./hooks";
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={Theme}>
-        <RoutesComponent />
-        <GlobalStyle />
-      </ThemeProvider>
+      <AppProvider>
+        <ThemeProvider theme={Theme}>
+          <Loading />
+          <RoutesComponent />
+          <GlobalStyle />
+          <ToastContainer theme="colored" />
+        </ThemeProvider>
+      </AppProvider>
     </>
   );
 }
