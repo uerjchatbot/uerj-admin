43f8161be25215e72484609057494c87d5e8e975
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-10-25 17:19:32 -0300

    feat: student teacher staff services

--- src/services/student/home.service.ts
+++ src/services/student/home.service.ts
@@ -1,8 +1,32 @@
+import { ICordinationData, IFfpData, IRepresentationData, ITeachersData } from "@/models/student";
 import { AxiosPromise } from "axios";
 import { api } from "../api";
 
 export const StudentServices = {
+  //? Home
   getHomeData(): AxiosPromise<any> {
-    return api.get("/student/question");
+    return api.get("student/question?identifier=STUDENT");
+  },
+
+  //? Teaching Staff Home
+  getTeachingStaffData(): AxiosPromise<any> {
+    return api.get("student/question?identifier=STUDENT_TEACHING_STAFF");
+  },
+
+  //? Teaching Staff Stages
+  getTeachingStaffFFPData(): AxiosPromise<IFfpData> {
+    return api.get("student/staff/name-link/question?identifier=STAFF_FFP");
+  },
+
+  getTeachingStaffCoordinationData(): AxiosPromise<ICordinationData> {
+    return api.get("student/staff/name-link/question?identifier=STAFF_COORDINATION");
+  },
+
+  getTeachingStaffRepresentationData(): AxiosPromise<IRepresentationData> {
+    return api.get("student/staff/name-link/question?identifier=STUDENT_TEACHING_STAFF");
+  },
+
+  getTeachingStaffTeacherData(): AxiosPromise<ITeachersData> {
+    return api.get("student/staff/name-link/question?identifier=STAFF_TEACHERS");
   }
 };
