17bede9dc26742a1dd7f1733b272fa9e37440df2
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-11-09 11:52:15 -0300

    feat: edit title

--- src/services/home/home.service.ts
+++ src/services/home/home.service.ts
@@ -5,5 +5,9 @@ import { api } from "../api";
 export const HomeServices = {
   getHomeData(): AxiosPromise<IHomeData> {
     return api.get("/question/1");
+  },
+
+  updateHomeData(title: string): AxiosPromise<IHomeData> {
+    return api.put("/question/1", { title });
   }
 };
