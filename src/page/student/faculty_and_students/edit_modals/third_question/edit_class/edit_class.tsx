a8dc46b4d3327d00e30b9dd9174e502ac8d04b5a
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-11-30 15:33:54 -0300

    refactor: remove unused import

--- src/page/student/faculty_and_students/edit_modals/third_question/edit_class/edit_class.tsx
+++ src/page/student/faculty_and_students/edit_modals/third_question/edit_class/edit_class.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { BsFillTrashFill } from "react-icons/bs";
 import { toast } from "react-toastify";
 
