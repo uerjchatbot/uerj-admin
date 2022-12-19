e0682b57329030952edc50be352284b3c47572d7
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-12-19 14:27:53 -0300

    refactor: optional data

--- src/models/matters.ts
+++ src/models/matters.ts
@@ -1,21 +1,19 @@
 export interface IMattersChildrenData {
   id: number;
-  question: string;
+  question?: string;
   father_question: number;
   title: string;
   response: boolean;
-  childrens: IMattersChildrenData[];
+  childrens?: IMattersChildrenData[];
 }
 
 export interface IMattersHomeData {
   id: number;
-  question: string;
+  question?: string;
   father_question: number;
   title: string;
   response: boolean;
-  created_at: string;
-  updated_at: string;
-  childrens: IMattersChildrenData[];
+  childrens?: IMattersChildrenData[];
 }
 
 export interface IMatterData {
